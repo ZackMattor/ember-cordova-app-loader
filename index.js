720045d748c15a95dca768f278ece9875339b72a
Zack Mattor <zmattor@gmail.com>
2016-01-25 09:58:59 -0500

    stuff working!

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-/* jshint node: true */
-'use strict';
+var path = require('path');
+var fs   = require('fs');
+var mergeTrees = require('broccoli-merge-trees');
+var funnel = require('broccoli-funnel');
+
+var manifest = require('./lib/manifest');
 
 module.exports = {
-  name: 'ember-cordova-app-loader'
-};
+  name: 'broccoli-manifest',
+
+  config: function (env, baseConfig) {
+    var options = baseConfig.manifest || {};
+
+    var defaultOptions = {
+      enabled: env === 'production',
+      appcacheFile: "/manifest.json",
+      excludePaths: ['tests/', 'robots.txt'],
+      includePaths: [],
+      loadSortOptions: ['vendor'],
+      loadExtensions: ['js', 'css']
+    };
+
+    for (var option in defaultOptions) {
+      if (!options.hasOwnProperty(option)) {
+        options[option] = defaultOptions[option];
+      }
+    }
+
+    this.manifestOptions = options;
+  },
+
+  postprocessTree: function (type, tree) {
+    var options = this.manifestOptions;
+
+    if (type === 'all' && options.enabled) {
+      manifestTree = funnel(tree, {
+        exclude: options.excludePaths
+      });
+      return mergeTrees([tree, manifest(manifestTree, options)]);
+    }
+
+    return tree;
+  },
+
+  treeFor: function() {}
+}
